9ea6205914700bf8128e6e961b8b78e1c66f6383
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2012-04-23 19:39:55 +0200

    destroy factory after each test

--- bridge/src/test/java/org/openengsb/loom/java/ConnectorManagerTest.java
+++ bridge/src/test/java/org/openengsb/loom/java/ConnectorManagerTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertThat;
 import java.util.Collection;
 
 import org.hamcrest.CoreMatchers;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.openengsb.core.api.security.service.UserDataManager;
 import org.openengsb.loom.java.impl.OpenEngSB3DomainFactory;
@@ -12,10 +14,20 @@ import org.openengsb.loom.java.impl.OpenEngSB3DomainFactory;
 public class ConnectorManagerTest {
 
     private static final String baseURL = "failover:(tcp://localhost:6549)?timeout=6000";
+    private OpenEngSB3DomainFactory domainFactory;
+
+    @Before
+    public void setUp() throws Exception {
+        domainFactory = new OpenEngSB3DomainFactory(baseURL);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        domainFactory.destroy();
+    }
 
     @Test
     public void retrieveServiceProxy() throws Exception {
-        OpenEngSB3DomainFactory domainFactory = new OpenEngSB3DomainFactory(baseURL);
         UserDataManager userDataManager = domainFactory.getRemoteProxy(UserDataManager.class);
         Collection<String> userList = userDataManager.getUserList();
         assertThat(userList, CoreMatchers.hasItems("admin", "user"));
