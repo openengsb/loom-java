c778b14d19cbf3c5b0ef5453b94ca2c9428ab1a0
stefan scheiber <stfnsche@gmail.com>
2013-05-29 13:25:21 +0200

    use labs-endtoend lib to setup openengsb

--- bridge/src/test/java/org/openengsb/loom/java/ConnectorManagerTest.java
+++ bridge/src/test/java/org/openengsb/loom/java/ConnectorManagerTest.java
@@ -2,52 +2,70 @@ package org.openengsb.loom.java;
 
 import static org.junit.Assert.fail;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Enumeration;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
+import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.io.FileUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.rules.TemporaryFolder;
+import org.openengsb.labs.endtoend.distribution.extractor.DistributionExtractor;
+import org.openengsb.labs.endtoend.distribution.resolver.DistributionResolver;
+import org.openengsb.labs.endtoend.karaf.CommandTimeoutException;
+import org.openengsb.labs.endtoend.karaf.Karaf;
+import org.openengsb.labs.endtoend.testcontext.TestContext;
+import org.openengsb.labs.endtoend.testcontext.loader.TestContextLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ConnectorManagerTest extends ConnectorManagerUT {
     private static final Logger LOGGER = LoggerFactory.getLogger(ConnectorManagerTest.class);
-
-    private static final String OPENENGSB_VERSION = "3.0.0-SNAPSHOT";
-    private static final String OPENENGSB_FRAMEWORK = "openengsb-framework-" + OPENENGSB_VERSION;
-
-    private static final TemporaryFolder tmpFolder = new TemporaryFolder();
-
+    
     private static final int MAX_ATTEMPTS = 120;
     private static final int POLL_INTERVAL = 1000; // 1 second
-
+    private static final long DEFAULT_TIMEOUT = 2L;
+    
+    private static final TemporaryFolder tmpFolder = new TemporaryFolder();
+    private static TestContext testContext;
+    
     @BeforeClass
-    public static void setUpOpenEngSB() throws Exception {
-        File fullZIPPath = AetherUtil.downloadArtifact(OPENENGSB_VERSION);
+    public static void setupOpenEngSB() throws IOException, InterruptedException {
+        DistributionResolver dr = new DistributionResolver();
         tmpFolder.create();
-        LOGGER.info("unpacking openengsb to " + tmpFolder.getRoot().getAbsolutePath());
-        unzip(fullZIPPath, tmpFolder.getRoot());
-        File openengsbRoot = new File(tmpFolder.getRoot(), OPENENGSB_FRAMEWORK + "/");
-        LOGGER.info("injecting features");
-        FileUtils.copyURLToFile(ClassLoader.getSystemResource("features.xml"), new File(openengsbRoot,
-            "deploy/features.xml"));
-        File executable;
-        if (!isWindows()) {
-            executable = new File(openengsbRoot, "bin/openengsb");
-        } else {
-            executable = new File(openengsbRoot, "bin/openengsb.bat");
-        }
-        executable.setExecutable(true);
-        ProcessBuilder pb = new ProcessBuilder(executable.getAbsolutePath());
+        LOGGER.info("created tmp folder: " + tmpFolder.getRoot().getAbsolutePath());
+        DistributionExtractor ds = new DistributionExtractor(tmpFolder.getRoot());
+        TestContextLoader testContextLoader = new TestContextLoader(dr, ds);
+        testContextLoader.loadContexts();
+        testContext = testContextLoader.getDefaultTestContext();
+        testContext.setup();
+        
+        Karaf k = testContext.getDistribution().getKaraf();
         LOGGER.info("starting openengsb ...");
-        pb.start();
+        try {
+            k.start(2L, TimeUnit.MINUTES);
+        } catch (CommandTimeoutException e) {
+            fail("karaf failed to start within 2 minutes");
+        }
+        
+        // wait some more as karaf is not ready yet
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        installFeature(k, "openengsb-ports-jms");
+        installFeature(k, "openengsb-domain-example");
+        installFeature(k, "openengsb-connector-example");
+        
+//        try {
+//            String response = k.getShell().execute("feature:list -i", DEFAULT_TIMEOUT, TimeUnit.MINUTES);
+//            System.out.println(response);
+//        } catch (CommandTimeoutException e1) {
+//            e1.printStackTrace();
+//        }
+        
         URL url = new URL("http://localhost:8090/openengsb/");
         int i = 0;
         while (i++ < MAX_ATTEMPTS) {
@@ -65,49 +83,20 @@ public class ConnectorManagerTest extends ConnectorManagerUT {
         }
         fail("openengsb could not be started after " + i + " attempts");
     }
-
-    private static boolean isWindows() {
-        String os = System.getProperty("os.name").toLowerCase();
-        return (os.indexOf("win") >= 0);
+    
+    private static void installFeature(Karaf karaf, String feature) {
+        LOGGER.info("installing feature " + feature + " ...");
+        try {
+            karaf.getShell().execute("feature:install " + feature, DEFAULT_TIMEOUT, TimeUnit.MINUTES);
+        } catch (CommandTimeoutException e1) {
+            fail("failed to install feature " + feature);
+        }
     }
-
+    
     @AfterClass
-    public static void cleanupTmp() throws Exception {
-        LOGGER.info("shutting down ...");
-        executeBatchFile("stop");
-        Thread.sleep(30 * 1000);
-        LOGGER.info("deleting openengsb");
+    public static void cleanUp() {
+        testContext.teardown();
+        LOGGER.info("deleting tmp folder ...");
         tmpFolder.delete();
-    }
-
-    private static void executeBatchFile(String batchFileName) throws IOException {
-        File openengsbRoot = new File(tmpFolder.getRoot(), OPENENGSB_FRAMEWORK + "/");
-        File executable;
-        if (!isWindows()) {
-            executable = new File(openengsbRoot, "bin/" + batchFileName);
-        } else {
-            executable = new File(openengsbRoot, "bin/" + batchFileName + ".bat");
-        }
-        executable.setExecutable(true);
-        ProcessBuilder pb = new ProcessBuilder(executable.getAbsolutePath());
-        pb.start();
-    }
-
-    private static void unzip(File source, File target) throws IOException {
-        ZipFile zipFile = new ZipFile(source);
-        Enumeration<?> entries = zipFile.entries();
-        while (entries.hasMoreElements()) {
-            ZipEntry entry = (ZipEntry) entries.nextElement();
-            if (entry.isDirectory()) {
-                // Assume directories are stored parents first then children.
-                LOGGER.debug("Extracting directory: " + entry.getName());
-                // This is not robust, just for demonstration purposes.
-                (new File(entry.getName())).mkdir();
-                continue;
-            }
-            LOGGER.debug("Extracting file: " + entry.getName());
-            FileUtils.copyInputStreamToFile(zipFile.getInputStream(entry), new File(target, entry.getName()));
-        }
-        zipFile.close();
     }
 }
